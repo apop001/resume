a3d67bf5b629b790e1675d29cb6dae72571e461f
apop001 <50104647+apop001@users.noreply.github.com>
2019-05-01 16:41:05 +0800

    Create index.vue

---
+++ project_vue_huawei/huawei_vue/src/views/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="hw_index">
+        <my-header></my-header>
+        <my-footer></my-footer>
+    </div>
+</template>
+
+<script>
+    import myHeader from '@/components/header'
+    import myFooter from '@/components/footer'
+    export default{
+        data(){
+            return{
+
+            }
+        },
+        components:{myHeader,myFooter}
+    }
+</script>
+
+<style>
+
+</style>
+
+
+
