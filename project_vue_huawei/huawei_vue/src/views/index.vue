edead8ebb02d5f1dbaa631eb9cb0b7f34a100d69
apop001 <50104647+apop001@users.noreply.github.com>
2019-05-01 21:34:37 +0800

    添加了主页的副头部

--- project_vue_huawei/huawei_vue/src/views/index.vue
+++ project_vue_huawei/huawei_vue/src/views/index.vue
@@ -1,12 +1,14 @@
 <template>
     <div id="hw_index">
         <my-header></my-header>
+        <my-subHeader></my-subHeader>
         <my-footer></my-footer>
     </div>
 </template>
 
 <script>
     import myHeader from '@/components/header'
+    import mySubHeader from '@/components/sub_header'
     import myFooter from '@/components/footer'
     export default{
         data(){
@@ -14,7 +16,7 @@
 
             }
         },
-        components:{myHeader,myFooter}
+        components:{myHeader,myFooter,mySubHeader}
     }
 </script>
 
