52ad57136d46afccbb780a6e2015ef38c9667901
apop001 <50104647+apop001@users.noreply.github.com>
2019-05-01 16:40:22 +0800

    Create footer.vue

---
+++ project_vue_huawei/huawei_vue/src/components/footer.vue
@@ -0,0 +1,408 @@
+<template>
+    <footer id="hw-footer">
+        <!-- 第一行ul -->
+        <ul>
+            <li> <!-- <a href="javascript:;"></a> -->
+                <a href="javascript:;"><img src="http://127.0.0.1:3001/img/footer/footer_line11.png" alt=""></a>
+                <a href="javascript:;"><p>百强企业&nbsp;品质保证</p>  </a>
+            </li>
+            <li>
+                <a href="javascript:;"><img src="http://127.0.0.1:3001/img/footer/footer_line12.png" alt=""></a>
+                <a href="javascript:;"><p>7天退货&nbsp;15天换货</p></a>
+            </li>
+            <li>
+                <a href="javascript:;"><img src="http://127.0.0.1:3001/img/footer/footer_line13.png" alt=""></a>
+                <a href="javascript:;"><p>48元起免运费</p></a>
+            </li>
+            <li>
+                <a href="javascript:;"><img src="http://127.0.0.1:3001/img/footer/footer_line14.png" alt=""></a>
+                <a href="javascript:;"><p>1000家维修网店&nbsp;全国联保</p> </a>
+            </li>
+        </ul>
+        <!-- 第二行ul -->
+        <ul>
+            <!-- 左边的大li -->
+            <li>
+                <ul><!-- 里面有6个li -->
+                    <!-- 小li1 -->
+                    <li>
+                        <ul>
+                            <li>购物相关</li>
+                            <li><a href="javascript:;">购物指南</a></li>
+                            <li><a href="javascript:;">配送方式</a></li>
+                            <li><a href="javascript:;">支付方式</a></li>
+                            <li><a href="javascript:;">常见问题</a></li>
+                        </ul>
+                    </li>
+                    <!-- 小li2 -->
+                    <li>
+                        <ul>
+                            <li>保修与退货</li>
+                            <li><a href="javascript:;">保修政策</a></li>
+                            <li><a href="javascript:;">退换货政策</a></li>
+                            <li><a href="javascript:;">退换货流程</a></li>
+                            <li><a href="javascript:;">保修状态查询</a></li>
+                            <li><a href="javascript:;">配件防伪查询</a></li>
+                        </ul>
+                    </li>
+                    <!-- 小li3 -->
+                    <li>
+                        <ul>
+                            <li>维修与技术支持</li>
+                            <li><a href="javascript:;">售后网点</a></li>
+                            <li><a href="javascript:;">预约维修</a></li>
+                            <li><a href="javascript:;">手机寄修</a></li>
+                            <li><a href="javascript:;">备件价格查询</a></li>
+                            <li><a href="javascript:;">上门服务</a></li>
+                        </ul>
+                    </li>
+                    <!-- 小li4 -->
+                    <li>
+                        <ul>
+                            <li>特色服务</li>
+                            <li><a href="javascript:;">防伪查询</a></li>
+                            <li><a href="javascript:;">补购补偿</a></li>
+                            <li><a href="javascript:;">以旧换新</a></li>
+                            <li><a href="javascript:;">礼品包装</a></li>
+                        </ul>
+                    </li>
+                    <!-- 小li5 -->
+                    <li>
+                        <ul>
+                            <li>关于我们</li>
+                            <li><a href="javascript:;">公司介绍</a></li>
+                            <li><a href="javascript:;">华为商城简介</a></li>
+                            <li><a href="javascript:;">华为线下门店</a></li>
+                            <li><a href="javascript:;">荣耀线下门店</a></li>
+                            <li><a href="javascript:;">诚征英才</a></li>
+                            <li><a href="javascript:;">意见反馈</a></li>
+                        </ul>
+                    </li>
+                    <!-- 小li6 -->
+                    <li>
+                        <ul>
+                            <li>友情链接 <span>???</span>  </li>
+                            <li><a href="javascript:;">华为集团</a></li>
+                            <li><a href="javascript:;">华为CBG官网</a></li>
+                            <li><a href="javascript:;">荣耀官网</a></li>
+                            <li><a href="javascript:;">花粉俱乐部</a></li>
+                            <li><a href="javascript:;">网站地图</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <!-- 右边的大li -->
+            <li>
+                <div>
+                    <p>400-088-6888</p>
+                    <p>7x24小时客服热线(仅收市话费)</p>
+                    <a href="javascript:;"><img src="http://127.0.0.1:3001/img/footer/footer_headphone.png" alt="">在线客服</a>
+                    <ul>
+                        <li>关注Vmall：</li>
+                        <li>
+                            <a href="javascript:;"><img src="http://127.0.0.1:3001/img/footer/footer_line21.png" alt=""></a>
+                            <img src="http://127.0.0.1:3001/img/footer/footer_triangle.png" alt=""><!-- 小尖尖 -->
+                            <img src="http://127.0.0.1:3001/img/footer/footer_line20.jpg" alt=""><!-- 二维码 -->
+                        </li>
+                        <li>
+                            <a href="javascript:;"><img src="http://127.0.0.1:3001/img/footer/footer_line22.png" alt=""></a>
+                        </li>
+                        <li>
+                            <a href="javascript:;"><img src="http://127.0.0.1:3001/img/footer/footer_line23.png" alt=""></a>
+                        </li>
+                        <li>
+                            <a href="javascript:;"><img src="http://127.0.0.1:3001/img/footer/footer_line24.png" alt=""></a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+        <!-- 第三行ul -->
+        <ul>
+            <!-- 第一个大li -->
+            <li>
+                <a href="javascript:;"><img src="http://127.0.0.1:3001/img/footer/footer_line30.png" alt=""></a>
+            </li>
+            <!-- 第二个大li -->
+            <li>
+                <!-- 第一个横着的ul -->
+                <ul>
+                    <li>
+                        <span><a href="javascript:;">华为集团</a></span>
+                        <span>|</span>
+                    </li>
+                    <li>
+                        <span><a href="javascript:;">华为CBG官网</a></span>
+                        <span>|</span>
+                    </li>
+                    <li>
+                        <span><a href="javascript:;">荣耀官网</a></span>
+                        <span>|</span>
+                    </li>
+                    <li>
+                        <span><a href="javascript:;">花粉俱乐部</a></span>
+                        <span>|</span>
+                    </li>
+                    <li>
+                        <span><a href="javascript:;">华为应用市场</a></span>
+                        <span>|</span>
+                    </li>
+                    <li>
+                        <span><a href="javascript:;">EMUI</a></span>
+                        <span>|</span>
+                    </li>
+                    <li>
+                        <span><a href="javascript:;">华为终端云空间</a></span>
+                        <span>|</span>
+                    </li>
+                    <li>
+                        <span><a href="javascript:;">开发者联盟</a></span>
+                        <span>|</span>
+                    </li>
+                    <li>
+                        <span><a href="javascript:;">华为商城手机版</a></span>
+                        <span>|</span>
+                    </li>
+                    <li>
+                        <span><a href="javascript:;">网站地图</a></span>
+                        <!-- <span>|</span> -->
+                    </li>
+                </ul>
+                <!-- 第二个横着的ul -->
+                <ul><!-- <a href="javascript:;"></a> -->
+                    <li><a href="javascript:;">隐私声明</a></li>
+                    <li><a href="javascript:;">服务协议</a></li>
+                    <li><a href="javascript:;">COOKIES</a></li>
+                    <li>Copyright&nbsp;&copy;&nbsp;2012-2019&nbsp;华为终端有限公司&nbsp;版权所有</li>
+                    <li><a href="javascript:;">粤ICP备19015064号-4</a><span>|</span></li>
+                    <li>备案主体编号：32201911426827</li>
+                </ul>
+            </li>
+            <!-- 第三个大li -->
+            <li>
+                <ul>
+                    <li><a href="javascript:;"><img src="http://127.0.0.1:3001/img/footer/footer_line31.jpg" alt=""></a></li>
+                    <li><a href="javascript:;"><img src="http://127.0.0.1:3001/img/footer/footer_line32.jpg" alt=""></a></li>
+                </ul>
+            </li>
+        </ul>
+    </footer>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style>
+    #hw-footer{
+        background-color:#F9F9F9;
+    }
+    /* 第一行ul */
+    #hw-footer>ul:first-child{
+        display:flex; /*让ul下的每个li弹起来*/
+        padding:18px 32px;
+        border-bottom:1px solid #E5E5E5;
+    }
+    #hw-footer>ul:first-child>li{
+        /* border:1px solid red; */
+        width:25vw;
+        display:flex;/*让ul下的li下的图片和文字x轴方向弹起来，并且x轴与y轴方向都居中*/
+        justify-content:center;
+        align-items:center;
+    }
+    /* #hw-footer>ul:first-child>li>a{
+        
+    } */
+    #hw-footer>ul:first-child>li>a>img{
+        width:55px;
+        height:55px
+    }
+    #hw-footer>ul:first-child>li>a>p{
+        font-size:18px;
+        color:#3A3A3A;
+        padding:12px 0 0 5px;
+    }
+    /* 第二行ul */
+    #hw-footer>ul:nth-child(2){
+        display:flex;/* 让左边和右边的大li横向弹起来 */
+        padding-bottom:12px;
+        border-bottom:1px solid #E5E5E5;
+    }
+    #hw-footer>ul:nth-child(2)>li:first-child{ /* 左边的大li */
+        width:75vw;
+        /* padding-left:45px; */
+        padding:20px 20px 30px 45px;
+        border-right:1px solid #E5E5E5;
+    }
+    #hw-footer>ul:nth-child(2)>li:first-child>ul{ /* 左边的大li下的大ul(内有6个li) */
+        display:flex; /* 让6个li弹起来 */
+    }
+    #hw-footer>ul:nth-child(2)>li:first-child>ul>li{ /* 6个小li */
+        /* border:1px solid red; */
+        width:16%;
+        margin-left:30px;
+    }
+    #hw-footer>ul:nth-child(2)>li:first-child>ul>li>ul>li{
+        padding-bottom:4px;
+    }
+    #hw-footer>ul:nth-child(2)>li:first-child>ul>li>ul>li:first-child{
+        font-size:14px;
+        color:#333;
+        padding-bottom:6px;
+    }
+    #hw-footer>ul:nth-child(2)>li:first-child>ul>li>ul>li:not(:first-child)>a{
+        font-size:12px;
+        color:#777;
+    }
+    #hw-footer>ul:nth-child(2)>li:first-child>ul>li>ul>li:not(:first-child)>a:hover{
+        color:#CF0A2C;
+    }
+    /*#hw-footer>ul:nth-child(2)>li:nth-child(2){  右边的大li 
+
+    }*/
+    #hw-footer>ul:nth-child(2)>li:nth-child(2)>div{
+        width:20vw;
+        padding:30px 0 10px 10px;
+        display:flex;
+        flex-direction:column;
+        justify-content:center;
+        align-items:center;
+
+    }
+    #hw-footer>ul:nth-child(2)>li:nth-child(2)>div>a{
+        /* display:block; */
+
+        font-size:14px;
+        color:#fff;
+
+        width:170px;
+        height:34px;
+        border-radius:17px;
+        background-color:#333;
+
+        display:flex;
+        justify-content:center;
+        align-items:center;
+
+        margin-bottom:15px;
+    }
+    #hw-footer>ul:nth-child(2)>li:nth-child(2)>div>a:hover{
+        opacity:0.8;
+    }
+    #hw-footer>ul:nth-child(2)>li:nth-child(2)>div>a>img{
+        width:20px;
+        height:20px;
+    }
+    #hw-footer>ul:nth-child(2)>li:nth-child(2)>div>p:first-child{
+        font-size:24px;
+        font-weight:bold;
+        color:#575757;
+        margin:0;
+    }
+    #hw-footer>ul:nth-child(2)>li:nth-child(2)>div>p:nth-child(2){
+        font-size:14px;
+    }
+    #hw-footer>ul:nth-child(2)>li:nth-child(2)>div>ul{
+        display:flex;
+    }
+    #hw-footer>ul:nth-child(2)>li:nth-child(2)>div>ul>li:first-child{
+        font-size:14px;
+        padding-top:5px;
+    }
+    #hw-footer>ul:nth-child(2)>li:nth-child(2)>div>ul>li:nth-child(2){
+        position:relative;
+    }
+    #hw-footer>ul:nth-child(2)>li:nth-child(2)>div>ul>li:nth-child(2)>img:nth-child(2){
+        width:12px;
+        height:8px;
+        position:absolute;
+        top:28px;
+        left:10px;
+        display:none;
+    }
+    #hw-footer>ul:nth-child(2)>li:nth-child(2)>div>ul>li:nth-child(2)>img:nth-child(3){ /* 微信hover的二维码 */
+        width:104px;
+        height:104px;
+        border:6px solid #fff;
+        display:none;/* !!!! */
+
+        position:absolute;
+        top:32px;
+        left:-36px;
+    }
+    #hw-footer>ul:nth-child(2)>li:nth-child(2)>div>ul>li:nth-child(2):hover>img:nth-child(2),
+    #hw-footer>ul:nth-child(2)>li:nth-child(2)>div>ul>li:nth-child(2):hover>img:nth-child(3){
+        display:block;
+    }
+    #hw-footer>ul:nth-child(2)>li:nth-child(2)>div>ul>li>a>img{/* 微信、头条、微博、华为小图标 */
+        width:28px;
+        height:28px;
+    }
+    #hw-footer>ul:nth-child(2)>li:nth-child(2)>div>ul>li>a>img:hover{
+        opacity:0.8;
+    }
+    
+    /* 第三行大ul */
+    #hw-footer>ul:nth-child(3){
+        /* padding:20px 60px; */
+        padding:20px 60px 20px 90px;
+        display:flex;
+        /* justify-content:space-between; */
+        /* justify-content:center; */
+    }
+    #hw-footer>ul:nth-child(3)>li:first-child>a>img{
+        width:82px;
+        height:20px;
+        margin-right:15px;
+    }
+    #hw-footer>ul:nth-child(3)>li:nth-child(2){
+
+    }
+    #hw-footer>ul:nth-child(3)>li:nth-child(2)>ul{ /* 让两个横着的ul里面的li横起来 */
+        display:flex;
+    }
+    #hw-footer>ul:nth-child(3)>li:nth-child(2)>ul>li,
+    #hw-footer>ul:nth-child(3)>li:nth-child(2)>ul>li a{ /* 两个横着的ul里面所有带a标签的文字 */
+        font-size:12px;
+        color:#777;
+    }
+    #hw-footer>ul:nth-child(3)>li:nth-child(2)>ul>li a:hover{
+        color:#ca151d;
+    }
+    #hw-footer>ul:nth-child(3)>li:nth-child(2)>ul:first-child>li>span:first-child{
+        /* padding-right:3px; */
+        padding:0 5px;
+    }
+    #hw-footer>ul:nth-child(3)>li:nth-child(2)>ul:nth-child(2){ /* 第二行横着的ul */
+        /* padding-left:5px; */
+        margin-top:3px;
+    }
+    #hw-footer>ul:nth-child(3)>li:nth-child(2)>ul:nth-child(2)>li{
+        padding:0 5px;
+    }
+    #hw-footer>ul:nth-child(3)>li:nth-child(2)>ul:nth-child(2)>li span{
+        padding-left:6px;
+    }
+
+    #hw-footer>ul:nth-child(3)>li:nth-child(3){
+        padding-left:55px;
+    }
+    #hw-footer>ul:nth-child(3)>li:nth-child(3)>ul{ /* 让存放图片的两个小ul横起来 */
+        display:flex;
+    }
+    #hw-footer>ul:nth-child(3)>li:nth-child(3)>ul>li{
+        padding:0 5px;
+    }
+    #hw-footer>ul:nth-child(3)>li:nth-child(3)>ul>li>a>img{ /* 两个小图片 */
+        width:70px;
+        height:24px;
+    }
+</style>
+
+
